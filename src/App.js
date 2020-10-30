0c973f2ec08bd4c3032ddf84a6ddfe574b09da6b
Davide Talesco (IS-IE) <davide_talesco@trendmicro.com>
2020-10-30 17:32:42 +0000

    updated code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const x = 0;
-const y = 0;
-const z = 0;
-
 const App = () => {
-  let acl = new window.Accelerometer({ frequency: 30 });
-  // console.log(acl);
-  // let max_magnitude = 0;
-  // acl.addEventListener('activate', () => console.log('Ready to measure.'));
-  // acl.addEventListener('error', (error) => console.log(`Error: ${error.name}`));
-  // acl.addEventListener('reading', () => {
-  //   let magnitude = Math.hypot(acl.x, acl.y, acl.z);
-  //   if (magnitude > max_magnitude) {
-  //     max_magnitude = magnitude;
-  //     console.log(`Max magnitude: ${max_magnitude} m/s2`);
-  //   }
-  // });
-  // acl.start();
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
+
+  if ('Accelerometer' in window) {
+    let sensor = new Accelerometer();
+
+    sensor.addEventListener('reading', (e) => {
+      window.alert(e.target);
+      setX(e.target.x);
+      setY(e.target.y);
+      setZ(e.target.z);
+    });
+
+    sensor.start();
+  }
 
   return (
     <div>
