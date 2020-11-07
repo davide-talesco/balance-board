f7e63e9f1b74ed143c22d38b952e83df3968e214
Davide Talesco (IS-IE) <davide_talesco@trendmicro.com>
2020-11-07 22:36:20 +0000

    added basic computeBalance algorythm

--- src/App.js
+++ src/App.js
@@ -5,27 +5,48 @@ const App = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
+  const [balance, setBalance] = useState(false);
 
-  if ('Accelerometer' in window) {
-    let sensor = new Accelerometer();
+  // eslint-disable-next-line no-undef
+  let sensor = new Accelerometer();
 
-    sensor.addEventListener('reading', (e) => {
-      window.alert(e.target);
-      setX(e.target.x);
-      setY(e.target.y);
-      setZ(e.target.z);
-    });
+  sensor.addEventListener('reading', () => {
+    setX(sensor.x);
+    setY(sensor.y);
+    setZ(sensor.z);
 
-    sensor.start();
-  }
+    const isInBalance = computeBalance(sensor);
+    setBalance(isInBalance ? 'You are in balance' : 'noooooo');
+  });
+
+  sensor.start();
 
   return (
     <div>
       <div>
-        X = {x} Y = {y} Z = {z}
+        X = {x} <br></br>Y = {y} <br></br>Z = {z}
       </div>
+      <div>balance = {balance}</div>
     </div>
   );
 };
+
+// TODO need to introduce score system
+// TODO need to change state only after XXX ms
+// TODO how do I know if I am hitting the ground outside the X and Y coordinates
+function computeBalance(sensor) {
+  // its on balance if X and Y are between range min and max
+  const range = { min: -2.3, max: 2.3 };
+
+  const isXinRange = sensor.x > range.min && sensor.x < range.max;
+  const isYinRange = sensor.y > range.min && sensor.y < range.max;
+
+  // Z should be higher than 9.5
+  const isZInRange = sensor.z > 9.5;
+
+  const isInBalance = isXinRange && isYinRange && isZInRange;
+
+  return isInBalance;
+}
 
 render(<App />, document.getElementById('root'));
