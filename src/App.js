cf1bc3da59ad0581ca04932476a0273f7cf97f1a
Davide Talesco (IS-IE) <davide_talesco@trendmicro.com>
2020-11-08 22:35:02 +0000

    added debounce logic

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ const App = () => {
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
   const [balance, setBalance] = useState(false);
+  const [debouncedBalance, setDebouncedBalance] = useState(false);
+  const lastBalance = {
+    value: false,
+    count: 1,
+  };
 
   // eslint-disable-next-line no-undef
   let sensor = new Accelerometer();
 
   sensor.addEventListener('reading', () => {
-    setX(sensor.x);
-    setY(sensor.y);
-    setZ(sensor.z);
+    try {
+      setX(sensor.x);
+      setY(sensor.y);
+      setZ(sensor.z);
 
-    const isInBalance = computeBalance(sensor);
-    setBalance(isInBalance ? 'You are in balance' : 'noooooo');
+      const isInBalance = computeBalance(sensor);
+      setBalance(isInBalance ? 'OK' : 'NO');
+
+      if (isInBalance === lastBalance.value) {
+        lastBalance.count++;
+      } else {
+        lastBalance.value = isInBalance;
+        lastBalance.count = 1;
+      }
+
+      if (lastBalance.count > 1) {
+        setDebouncedBalance(isInBalance ? 'OK' : 'NO');
+      }
+    } catch (err) {
+      alert(err);
+    }
   });
 
   sensor.start();
@@ -27,13 +47,13 @@ const App = () => {
         X = {x} <br></br>Y = {y} <br></br>Z = {z}
       </div>
       <div>balance = {balance}</div>
+      <div>debouncedBalance = {debouncedBalance}</div>
     </div>
   );
 };
 
-// TODO need to introduce score system
 // TODO need to change state only after XXX ms
-// TODO how do I know if I am hitting the ground outside the X and Y coordinates
+
 function computeBalance(sensor) {
   const radius = 2.2;
   // https://www.youmath.it/formulari/formulari-di-geometria-analitica/426-distanza-tra-due-punti-nel-piano.html
